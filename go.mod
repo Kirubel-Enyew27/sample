87cc9a5670f3af534f084ccaebc132b0f68c511d
Kira37 <kirubelenyew76@gmail.com>
2025-02-12 10:10:27 +0300

    intial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Kirubel-Enyew27/sample
+
+go 1.21.6
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using default environment variables.")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default if PORT isn't set
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Kira!")
+	})
+
+	log.Printf("Starting server on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
